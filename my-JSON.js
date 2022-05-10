9f613281bf1e240e1e9dde4aa16a5486c95028e0
Carl Brubaker <developing.rust@gmail.com>
2022-05-10 16:04:02 -0400

    added mergeJSON

--- my-JSON.js
+++ my-JSON.js
@@ -3,7 +3,33 @@
  * my-JSON.js
  * Copyright (c) 2022 by Carl David Brubaker
  * All Rights Reserved
- * Version 0.0.1
+ * Version 1.0.0
  *
  * Utility functions to work with JSON.
  */
+
+/**
+ * Merges JSON files into an object, overwritting duplicate keys.
+ * @async
+ * @param {string} changeFile JSON Overwrites.
+ * @param {string} defaultFile JSON defaults.
+ * @param {bool} filePaths If changeFile and defaultFile are paths.
+ * @returns {Promise<object>} Combined object of default and overwritten JSON.
+ */
+async function mergeJSON(changeJSON, defaultJSON, filePaths = false) {
+	if (filePaths) {
+		changeJSON = require(changeJSON);
+		defaultJSON = require(defaultJSON);
+	}
+
+	const defaultKeys = Object.keys(defaultJSON);
+	let finalJSON = {};
+
+	for await (const key of defaultKeys) {
+		finalJSON[key] = key in changeJSON ? changeJSON[key] : defaultJSON[key];
+	}
+
+	return finalJSON;
+}
+
+module.exports.mergeJSON = mergeJSON;
