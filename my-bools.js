bbcde7fef0b6c9da1c3c2fab6ae28266ebf22b0c
Carl Brubaker <developing.rust@gmail.com>
2022-07-06 16:08:05 -0400

    added objectWithProperty. Tests pass

--- my-bools.js
+++ my-bools.js
@@ -3,14 +3,20 @@
  * my-bools.js
  * Copyright (c) 2022 by Carl David Brubaker
  * All Rights Reserved
- * Version 1.1.0
+ * Version 1.2.0
  *
  * Utility functions that return true or false.
  * const is = require(`./my-bools`);
  *
  * * objectOrNull(arg)
+ * * objectWithProperty(arg, property)
  */
 
+/**
+ * Checks whether the supplied argument is an object or null.
+ * @param {*} arg Argument to check.
+ * @returns {boolean} If arg is an object or null.
+ */
 function objectOrNull(arg) {
 	if (arg === null || (typeof arg === `object` && !Array.isArray(arg))) {
 		return true;
@@ -19,3 +25,28 @@ function objectOrNull(arg) {
 }
 
 module.exports.objectOrNull = objectOrNull;
+
+/**
+ * Checks whether supplied argument is an object and has given property
+ * @param {*} arg Argument to check.
+ * @param {string} property Name of property to check for.
+ * @returns {boolean} If arg is object and has given property.
+ * @throws {TypeError} If property is not a string.
+ */
+function objectWithProperty(arg, property) {
+	if (typeof property !== `string`) {
+		throw TypeError(`property must be a string`);
+	}
+
+	if (
+		typeof arg === `object` &&
+		!Array.isArray(arg) &&
+		arg !== null &&
+		arg.hasOwnProperty(property)
+	) {
+		return true;
+	}
+	return false;
+}
+
+module.exports.objectWithProperty = objectWithProperty;
--- tests/my-bools.test.js
+++ tests/my-bools.test.js
@@ -17,7 +17,7 @@ const is = require(`./../my-bools`);
 // is.objectOrNull(arg)
 // ****************************************
 describe(`is.objectOrNull`, () => {
-	const testArgs = [
+	test.each([
 		{type: `an object`, arg: {}, expected: true},
 		{type: `null`, arg: null, expected: true},
 		{type: `a string`, arg: `abc`, expected: false},
@@ -25,10 +25,53 @@ describe(`is.objectOrNull`, () => {
 		{type: `false`, arg: false, expected: false},
 		{type: `a number`, arg: 123, expected: false},
 		{type: `an array`, arg: [], expected: false},
-	];
-	test.each(testArgs)(`should return $expected if arg is $type`, ({type, arg, expected}) => {
+	])(`should return $expected if arg is $type`, ({type, arg, expected}) => {
 		const result = is.objectOrNull(arg);
 		expect(result).toBe(expected);
 		expect(typeof result).toBe(`boolean`);
+	});
+});
+
+// ****************************************
+// is.objectWithProperty(arg, property)
+// ****************************************
+describe(`is.objectWithProperty`, () => {
+	const arg = {hasProperty: `yes`};
+
+	// ######## arg ########
+	test.each([
+		{type: `an empty object`, value: {}, expected: false},
+		{type: `an object with property`, value: {hasProperty: `yes`}, expected: true},
+		{type: `an object without property`, value: {hasNotProperty: `no`}, expected: false},
+		{type: `null`, value: null, expected: false},
+		{type: `true`, value: true, expected: false},
+		{type: `false`, value: false, expected: false},
+		{type: `an array`, value: [], expected: false},
+		{type: `a string`, value: `abc`, expected: false},
+		{type: `a number`, value: 123, expected: false},
+	])(`should return $expected when arg is $type`, ({type, value, expected}) => {
+		const result = is.objectWithProperty(value, `hasProperty`);
+		expect(result).toBe(expected);
+		expect(typeof result).toBe(`boolean`);
+	});
+
+	// ######## property ########
+	test(`should not throw an error if property is a string`, () => {
+		expect(() => {
+			is.objectWithProperty(arg, `hasProperty`);
+		}).not.toThrowError();
+	});
+
+	test.each([
+		{type: `a number`, value: 123},
+		{type: `an object`, value: {}},
+		{type: `true`, value: true},
+		{type: `false`, value: false},
+		{type: `null`, value: null},
+		{type: `an array`, value: []},
+	])(`should throw an error if property is $type`, ({type, value}) => {
+		expect(() => {
+			is.objectWithProperty(arg, value);
+		}).toThrow(TypeError(`property must be a string`));
 	});
 });
