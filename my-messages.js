1958293ec6185cac3d79e0fffd26c2cd44c5fcc7
Carl Brubaker <developing.rust@gmail.com>
2022-09-12 07:38:16 -0400

    reordered ny-messages

--- my-messages.js
+++ my-messages.js
@@ -12,7 +12,7 @@
  */
 
 const is = require(`./my-bools`);
-
+// TODO: Reorganize with better nameing. Match test data.types
 // ############################################################
 // Error Messages
 // ############################################################
@@ -23,33 +23,23 @@ const is = require(`./my-bools`);
 // Used for exporting
 let typeErrors = {};
 
+// ------------------------------
+// Singular Types
+// ------------------------------
 /**
  * Returns TypeError with uniform message.
  * @param {string} arg Argument name that should be a string.
  * @returns {TypeError}
  * @throws {TypeError} If $arg is not boolean.
  */
-function booleanTypeError(arg) {
+
+function isNotBooleanTypeError(arg) {
 	if (!is.string(arg)) {
 		throw stringTypeError(arg);
 	}
 	return TypeError(`$${arg} must be true or false!`);
 }
-typeErrors.boolean = booleanTypeError;
-
-/**
- * Returns TypeError with uniform message.
- * @param {string} arg Argument name that should be a string.
- * @returns {TypeError}
- * @throws {TypeError} If $arg is not a string.
- */
-function nullStringOrArrayOfStringsTypeError(arg) {
-	if (!is.string(arg)) {
-		throw stringTypeError(arg);
-	}
-	return TypeError(`$${arg} must be a string, string[], or null!`);
-}
-typeErrors.nullStringOrArrayOfStrings = nullStringOrArrayOfStringsTypeError;
+typeErrors.isNotBoolean = isNotBooleanTypeError;
 
 /**
  * Returns TypeError with uniform message.
@@ -57,13 +47,32 @@ typeErrors.nullStringOrArrayOfStrings = nullStringOrArrayOfStringsTypeError;
  * @returns TypeError
  * @throws TypeError if $arg is not a string.
  */
-function stringTypeError(arg) {
+function isNotStringTypeError(arg) {
 	if (!is.string(arg)) {
 		throw TypeError(`$arg must be a string!`);
 	}
 
 	return TypeError(`$${arg} must be a string!`);
 }
-typeErrors.string = stringTypeError;
+typeErrors.isNotString = isNotStringTypeError;
+
+// ------------------------------
+// Compound Types
+// ------------------------------
+/**
+ * Returns TypeError with uniform message.
+ * @param {string} arg Argument name that should be a string.
+ * @returns {TypeError}
+ * @throws {TypeError} If $arg is not a string.
+ */
+function isNotArrayOfStringsStringOrNullTypeError(arg) {
+	if (!is.string(arg)) {
+		throw isNotStringTypeError(arg);
+	}
+	return TypeError(`$${arg} must be a string[], string, or null!`);
+}
+typeErrors.isNotArrayOfStringsStringOrNull = isNotArrayOfStringsStringOrNullTypeError;
+
+// TODO: objectOrNullTypeError func
 
 module.exports.typeError = typeErrors;
