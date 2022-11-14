66da946e880f7c7a9f49c71f5ac57b1d260e52ca
Carl Brubaker <developing.rust@gmail.com>
2022-11-14 07:15:55 -0500

    Added Date type

--- tests/test-data/type-testing.js
+++ tests/test-data/type-testing.js
@@ -13,6 +13,7 @@
 const anArray = {type: `an array`, arg: []};
 const anArrayOfStrings = {type: `an array of strings`, arg: [`123`, `123`]};
 const anArrayOfNonStrings = {type: `an array of non-strings`, arg: [`123`, 123]};
+const aDate = {type: `a date`, arg: new Date()};
 const isFalse = {type: `false`, arg: false};
 const isNull = {type: `null`, arg: null};
 const aNumber = {type: `a number`, arg: 123};
@@ -25,28 +26,54 @@ let dataTypes = {
 	// ############################################################
 	// Singular Types
 	// ############################################################
-	isNotArray: [isFalse, isNull, aNumber, anObject, aString, isTrue],
+	isNotArray: [isFalse, isNull, aNumber, anObject, aString, isTrue, aDate],
 	isBoolean: [isTrue, isFalse],
-	isNotBoolean: [isNull, aString, aNumber, anObject, anArray],
-	isNotNumber: [isTrue, isFalse, aString, anArray, anObject, isNull],
-	isNotObject: [isTrue, isFalse, aString, aNumber, anArray, isNull],
-	isNotString: [aNumber, anObject, isNull, anArray, isTrue, isFalse],
+	isNotBoolean: [isNull, aString, aNumber, anObject, anArray, aDate],
+	isNotDate: [anArray, isFalse, isNull, aNumber, anObject, aString, isTrue],
+	isNotNumber: [isTrue, isFalse, aString, anArray, anObject, isNull, aDate],
+	isNotObject: [isTrue, isFalse, aString, aNumber, anArray, isNull, aDate],
+	isNotString: [aNumber, anObject, isNull, anArray, isTrue, isFalse, aDate],
 
 	// ############################################################
 	// Compound Types
 	// ############################################################
-	isNotArrayOfStrings: [isFalse, isNull, aNumber, anObject, aString, isTrue, anArrayOfNonStrings],
+	isNotArrayOfStrings: [
+		isFalse,
+		isNull,
+		aNumber,
+		anObject,
+		aString,
+		isTrue,
+		anArrayOfNonStrings,
+		aDate,
+	],
 	isArrayOfStringsOrNull: [anArrayOfStrings, isNull],
-	isNotArrayOfStringsOrNull: [isFalse, aNumber, anObject, aString, isTrue, anArrayOfNonStrings],
+	isNotArrayOfStringsOrNull: [
+		isFalse,
+		aNumber,
+		anObject,
+		aString,
+		isTrue,
+		anArrayOfNonStrings,
+		aDate,
+	],
 	isArrayOfStringsOrString: [aString, anArrayOfStrings],
-	isNotArrayOfStringsOrString: [aNumber, anObject, anArrayOfNonStrings, isTrue, isFalse, isNull],
+	isNotArrayOfStringsOrString: [
+		aNumber,
+		anObject,
+		anArrayOfNonStrings,
+		isTrue,
+		isFalse,
+		isNull,
+		aDate,
+	],
 	isArrayOfStringsStringOrNull: [isNull, aString, anArrayOfStrings],
-	isNotArrayOfStringsStringOrNull: [aNumber, anObject, anArrayOfNonStrings, isTrue, isFalse],
+	isNotArrayOfStringsStringOrNull: [aNumber, anObject, anArrayOfNonStrings, isTrue, isFalse, aDate],
 	isObjectOrNull: [isNull, anObject],
-	isNotObjectOrNull: [aNumber, anArray, isTrue, isFalse, aString],
+	isNotObjectOrNull: [aNumber, anArray, isTrue, isFalse, aString, aDate],
 	isObjectOrString: [anObject, aString],
-	isNotObjectOrString: [isNull, aNumber, isTrue, isFalse, anArray],
+	isNotObjectOrString: [isNull, aNumber, isTrue, isFalse, anArray, aDate],
 	isStringOrNull: [aString, isNull],
-	isNotStringOrNull: [anArray, isFalse, anObject, isTrue, aNumber],
+	isNotStringOrNull: [anArray, isFalse, anObject, isTrue, aNumber, aDate],
 };
 module.exports.type = dataTypes;
